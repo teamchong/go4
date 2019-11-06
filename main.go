4e67e5671079a7a6adfd1fd5c42774de4fa070b1
Steven Chong <steven@teamchong.com>
2019-11-06 15:33:47 +0000

    go get

--- main.go
+++ main.go
@@ -18,12 +18,12 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
+	// router.LoadHTMLGlob("templates/*.tmpl.html")gi
 	router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	// router.GET("/", func(c *gin.Context) {
+	// 	c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	// })
 
 	router.Run(":" + port)
 }
